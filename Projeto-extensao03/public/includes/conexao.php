74550c5d612bcc488d52727f553d387cb9ead716
CAbomini <cabomini0@gmail.com>
2026-05-11 20:08:52 -0300

    Update conexao.php

--- Projeto-extensao03/public/includes/conexao.php
+++ Projeto-extensao03/public/includes/conexao.php
@@ -6,6 +6,8 @@ $database = "meu_banco";
 
 $conn = new mysqli($host, $user, $password, $database);
 
+$conn->set_charset("utf8mb4");
+
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
